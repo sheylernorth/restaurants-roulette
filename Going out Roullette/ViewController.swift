69cd22a9761a2b4b90c81d6b0a171b3076da19e4
Sheyler <sheyler@Sheylers-MacBook-Pro.local>
2022-08-25 16:18:45 -0600

    first commit

---
+++ Going out Roullette/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Going out Roullette
+//
+//  Created by Sheyler on 8/14/22.
+//  Copyright © 2022 Sheyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
